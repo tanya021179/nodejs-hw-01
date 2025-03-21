d45e436e2ccc6d1ebe38b0df2ccd6b5584731bde
Tetiana Prokopenko <tanya021179@gmail.com>
2025-03-21 14:36:30 +0100

    nodejs

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,11 @@
-export const addOneContact = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+import { createFakeContact } from "../utils/createFakeContact.js";
+
+export const addOneContact = async () => {
+    const contactList = await readContacts();
+    const addContact = createFakeContact();
+    writeContacts([...contactList, addContact]);
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,13 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+const generateContacts = async (number) => {
+    const contactList = await readContacts();
+    const newContact = Array(number).fill(0).map(createFakeContact);
+    await writeContacts([...contactList, ...newContact]);
+    // console.log(newContact);
+
+ };
 
 generateContacts(5);
